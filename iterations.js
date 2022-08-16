e668a214fe5bda16e17f16368897289ae64975f7
AsyaPolyak <asyapolyachka@gmail.com>
2022-08-16 16:47:31 +0300

    added a task with an asterisk (triangle)

---
+++ iterations.js
@@ -0,0 +1,34 @@
+/*let num =50;
+for(let i = 1; i<=5; i++){
+    console.log(num);
+    num++;
+}
+for(let i = 1; i<10; i++){
+    if(i===6){
+        //break;
+        continue;//пропустить значення
+    }
+    console.log(i);
+}*/
+//Цикл в циклі
+/*for(let i = 0; i< 3; i++){
+    console.log(i);
+    for(let j=0; j<3; j++){
+        console.log(j);
+    }
+}*/
+//*
+//**
+//***
+//****
+//*****
+//******
+/*let result='';
+let lenght =7;
+
+for(let i = 1; i < lenght; i++){
+    for(let j=0; j < i; j++){
+        result+='*';
+    }
+    result+='\n';
+}console.log(result);*/
