93590456db1a81cb802cb77b4d9ecb2573bcd46d
AsyaPolyak <asyapolyachka@gmail.com>
2022-08-25 13:21:20 +0300

    added function getCoupeNumber

---
+++ method.js
@@ -0,0 +1,10 @@
+let str = "Hello world!";
+console.log(str.substr(6, 5));
+
+
+function calculateVolumeAndArea(sqr) {
+    const result = Math.pow(sqr);
+    return result;
+
+}
+console.log(calculateVolumeAndArea(5));
