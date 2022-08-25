b9c77db9fb23c09298d751beae689b65c600d64d
AsyaPolyak <asyapolyachka@gmail.com>
2022-08-25 17:43:32 +0300

    added function findMaxNumber

--- function_1.js
+++ function_1.js
@@ -32,3 +32,16 @@ console.log();
 console.log();
 // которая принимает в себя 4 числа и возвращает самое большее из них.
 //Если один из аргументов не является числом или их меньше 4 - возвращается 0. Дробные числа разрешены.
+function findMaxNumber(num1, num2, num3, num4) {
+    if(typeof(num1) !== 'number'||
+       typeof(num2) !== 'number'||
+       typeof(num3) !== 'number'||
+       typeof(num4) !== 'number'
+    ){return 0;}
+    else{
+        return Math.max(num1, num2, num3, num4);
+    }
+
+}
+console.log(findMaxNumber(1, 5, 6.6, 11));// =>  11
+console.log(findMaxNumber(1, 5, '6', '10'));//=>  0
