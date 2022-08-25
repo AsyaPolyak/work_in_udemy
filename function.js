722d6f624e3dced3ae88f62318e905a7fd79a2f9
AsyaPolyak <asyapolyachka@gmail.com>
2022-08-25 13:22:13 +0300

    added function getCoupeNumber

--- function.js
+++ function.js
@@ -18,4 +18,45 @@ let str='';
     }
     return str;
 }
-console.log(getMathResult(10, 5));
+console.log(getMathResult(5, 3));
+
+//обэм і площа поверхності куба
+function calculateVolumeAndArea(num) {
+    
+    if(!Number.isInteger(num) || num < 0 || typeof(num) !== 'number'){
+        return `При вычислении произошла ошибка`;
+    }
+    else {
+       let v =0, s=0;
+       v = num *num *num;
+       s = 6*(num * num) ;
+        return `Объем куба: ${v}, площадь всей поверхности: ${s}`;
+    }
+    }
+   console.log( calculateVolumeAndArea(5));
+   console.log( calculateVolumeAndArea(15));
+   console.log( calculateVolumeAndArea(15.5));
+   console.log( calculateVolumeAndArea('15'));// => 'При вычислении произошла ошибка'
+   console.log( calculateVolumeAndArea(-15));//  => 'При вычислении произошла ошибка'
+//задача про вагони
+function getCoupeNumber(num) {
+    if(typeof(num) !=='number' || num < 0 || !Number.isInteger(num)) {
+        return "Ошибка. Проверьте правильность введенного номера места";
+    }
+    else if (num ===0 || num>36) {
+       return  "Таких мест в вагоне не существует";
+    }
+    for (let i=4; i<=36; i+=4){
+        if (num <=i){
+          return Math.ceil(num/4);   
+        }
+        
+    }
+}
+console.log(getCoupeNumber(33));//  => 9
+console.log(getCoupeNumber(7));//  => 2
+console.log(getCoupeNumber(300));//  => "Таких мест в вагоне не существует"
+console.log(getCoupeNumber(0));//  => "Таких мест в вагоне не существует"
+console.log(getCoupeNumber(7.7));//  => "Ошибка. Проверьте правильность введенного номера места"
+console.log(getCoupeNumber(-10));//  => "Ошибка. Проверьте правильность введенного номера места"
+console.log(getCoupeNumber('Hello'));//  => "Ошибка. Проверьте правильность введенного номера места"
