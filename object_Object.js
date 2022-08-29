ccc6138dd0ad2f8f3b65517c8dcb93d350217a8b
AsyaPolyak <asyapolyachka@gmail.com>
2022-08-29 11:01:51 +0300

    added counter

--- object_Object.js
+++ object_Object.js
@@ -13,12 +13,25 @@ console.log(obj["colors"]["border"]);
 //console.log(obj.name);
 //delete obj.name;
 //console.log(obj);
+
 for(let key in obj){
     if(typeof(obj[key]) === 'object') {
         for (let i in obj[key]) {
             console.log(`Свойство ${i} має значення ${obj[key][i]}`);
+            
         }
     } else {
     console.log(`Свойство ${key} має значення ${obj[key]}`);
+    //counter++;
     }
 }
+//Порахувати скільки ключей в обекті (в Object не має свойства lenght)**
+
+
+//додати counter++ до if
+let counter =0;
+for(let key in obj){
+    counter++;
+
+}
+console.log(counter);
