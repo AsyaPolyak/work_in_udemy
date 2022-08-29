ebb404639d7a5b7a712c60435e7173739fd1ed4b
AsyaPolyak <asyapolyachka@gmail.com>
2022-08-29 10:55:54 +0300

    added object

---
+++ fibonacci_number.js
@@ -0,0 +1,22 @@
+//Задача с собеседований на числа Фибоначчи
+function fib(num) {
+    if(typeof(num) !== 'number' ||num <=0 || !Number.isInteger(num)){
+        return '';
+    }
+    let result= '';
+    let first= 0;
+    let second = 1;
+    for(let i = 0; i< num; i++) {
+        if(i + 1 === num){
+            result+= `${first}`;
+        } else {
+            result+= `${first} `;
+        }
+        let third =first +second;
+        first = second;
+        second = third;
+    }
+    return result;
+}
+console.log(fib(5));
+console.log(fib(0));
---
+++ object_Object.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const obj ={
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    }
+};
+console.log(obj["colors"]["border"]);
+//console.log(obj.name);
+//delete obj.name;
+//console.log(obj);
+for(let key in obj){
+    if(typeof(obj[key]) === 'object') {
+        for (let i in obj[key]) {
+            console.log(`Свойство ${i} має значення ${obj[key][i]}`);
+        }
+    } else {
+    console.log(`Свойство ${key} має значення ${obj[key]}`);
+    }
+}
