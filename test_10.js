fee9210496a99f9543c65d5992a51951f2a528a2
AsyaPolyak <asyapolyachka@gmail.com>
2022-08-30 13:07:35 +0300

    added function showProgrammingLangs

--- test_10.js
+++ test_10.js
@@ -15,6 +15,17 @@ function showExperience(plan) {
  const {exp} = plan.skills;
  return exp;
 }
-    
-  
 console.log(showExperience(personalPlanPeter));
+
+function showProgrammingLangs(plan) {
+    let str ='';
+
+    const {programmingLangs} = plan.skills;
+
+    for(let key in programmingLangs){
+        str +=`Язык ${key} изучен на ${programmingLangs[key]}\n`
+    }
+
+    return str;
+}
+console.log(showProgrammingLangs(personalPlanPeter));//"Язык js изучен на 20% Язык php изучен на 10%"
