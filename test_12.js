866b294a55b11e8e68b920b4a8c1887b112ca2c5
AsyaPolyak <asyapolyachka@gmail.com>
2022-08-30 18:28:29 +0300

    added function availableCurr

--- test_12.js
+++ test_12.js
@@ -9,3 +9,24 @@ function reverse(str){
     return str.split('').reverse().join('');
 }
 console.log(reverse(someString));//'gnirts egnarts emos si sihT'
+//У вас есть банкомат, который выдает деньги из двух разных банков в разных валютах. Один банк основной
+//с базовыми валютами, второй дополнительный с прочими валютами:
+//функцию банкомата availableCurr, которая принимает два аргумента:
+//первый - это массив со всеми доступными валютами из двух банков сразу 
+//(сейчас представим, что они не могут повторяться), второй - необязательный аргумент, который указывает ту валюту, которая сейчас закончилась в банкомате. Если массив в
+//первом аргументе пустой - то функция возвращает строку 'Нет доступных валют'. Функция возвращает строку в нужном виде.
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+
+function availableCurr(arr, missingCurrent) {
+    let str ='';
+    arr.length === 0 ? str= `Нет доступных валют` : str =`Доступные валюты:\n`;
+    
+    arr.forEach(function(curr, i){
+        if(curr !== missingCurrent){
+            str+=`${curr}\n`;
+        }
+    })
+    return str;
+}
+console.log(availableCurr([...baseCurrencies, ...additionalCurrencies], 'CNY'));
